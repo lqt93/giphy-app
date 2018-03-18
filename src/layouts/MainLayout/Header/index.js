22312378afc7f064910954fb3ba78ddb4735da75
Thinh Le <lqthinh93@gmail.com>
2018-03-19 00:32:27 +0700

    add header and modify position of body

---
+++ src/layouts/MainLayout/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.scss'
+
+class MainHeader extends React.Component {
+  render () {
+    return (
+      <div className='main-header'>
+        <div className='brand-name'> Giphy App </div>
+      </div>
+    )
+  }
+}
+
+export default MainHeader
--- src/layouts/MainLayout/index.js
+++ src/layouts/MainLayout/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import './MainLayout.scss'
-
+import Header from './Header'
 
 export const MainLayout = ({ component: Component, store, route, ...rest }) => {
   return (
     <Route exact={route.exact} {...rest} render={props => (
       <div className='main-layout'>
+        <Header />
         <Component {...props} store={store} />
       </div>
     )} />
